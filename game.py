c3c2d86cdefa78d2896afab74f180cd469168b4d
MartinMosqueira <tinchomosqueira@gmail.com>
2023-12-05 11:45:03 -0300

    fix(Project-C2): :art: Motion Enhancement
    Improved movement by adding vectors and rotated sprites to correct direction.

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 
 # Initialize pygame
@@ -8,7 +9,7 @@ ancho = 1000
 alto = 600
 
 # window
-window = pygame.display.set_mode((1000,600))
+window = pygame.display.set_mode((1000, 600))
 
 # title and icon
 pygame.display.set_caption("Hello World")
@@ -30,6 +31,8 @@ clock = pygame.time.Clock()
 running = True
 player_pos = pygame.Vector2(window.get_width() / 2, window.get_height() / 2)
 dt = 0
+direction = pygame.Vector2(0, 0)  # Inicialmente sin movimiento
+angle = 0  # Inicialmente sin rotación
 
 while running:
     for event in pygame.event.get():
@@ -39,25 +42,44 @@ while running:
 
     window.fill(color)
 
-    # Draw cycle orange
-    window.blit(playerOrange, player_pos)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
     grosor_borde = 10
     pygame.draw.rect(window, edges, (0, 0, ancho, grosor_borde))  # Borde superior
     pygame.draw.rect(window, edges, (0, alto - grosor_borde, ancho, grosor_borde))  # Borde inferior
     pygame.draw.rect(window, edges, (0, 0, grosor_borde, alto))  # Borde izquierdo
     pygame.draw.rect(window, edges, (ancho - grosor_borde, 0, grosor_borde, alto))  # Borde derecho
-    pygame.display.update()
+
+    # Draw rotated cycle orange
+    rotated_player = pygame.transform.rotate(playerOrange, angle)
+    player_rect = rotated_player.get_rect(center=player_pos)
+    window.blit(rotated_player, player_rect.topleft)
+
+    keys = pygame.key.get_pressed()
+    new_direction = pygame.Vector2(0, 0)
+    if keys[pygame.K_w]:
+        new_direction.y = -1
+    if keys[pygame.K_s]:
+        new_direction.y = 1
+    if keys[pygame.K_a]:
+        new_direction.x = -1
+    if keys[pygame.K_d]:
+        new_direction.x = 1
+
+    # Si se ha pulsado alguna tecla de movimiento, actualizar la dirección
+    if new_direction.length() != 0:
+        direction = new_direction
+
+    # No permite movimientos en diagonal
+    if new_direction.x and new_direction.y != 0:
+        direction = pygame.Vector2(0, 0)
+
+    # Actualizar ángulo y posición del jugador
+    angle = direction.angle_to(pygame.Vector2(0, -1))  # Calcular ángulo con respecto a arriba
+    player_pos += direction * 300 * dt
+
+    # Limites de la pantalla
+    player_pos.x = max(30, min(player_pos.x, ancho - 30))
+    player_pos.y = max(30, min(player_pos.y, alto - 30))
+
     pygame.display.update()
 
     dt = clock.tick(60) / 1000
